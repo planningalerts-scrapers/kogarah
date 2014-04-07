7ae4d154ce67e7ebfc3bc8d87317f05a4f8b3257
Matthew Landauer <matthew@openaustralia.org>
2014-04-08 07:34:39 +1000

    Go through all pages

--- scraper.rb
+++ scraper.rb
@@ -18,6 +18,22 @@ def scrape_page(page)
   end
 end
 
+# Implement a click on a link that understands stupid asp.net doPostBack
+def click(page, doc)
+  href = doc["href"]
+  if href =~ /javascript:__doPostBack\(\'(.*)\',\'(.*)'\)/
+    event_target = $1
+    event_argument = $2
+    form = page.form_with(id: "aspnetForm")
+    form["__EVENTTARGET"] = event_target
+    form["__EVENTARGUMENT"] = event_argument
+    form.submit
+  else
+    # TODO Just follow the link likes it's a normal link
+    raise
+  end
+end
+
 url = "http://www2.kogarah.nsw.gov.au/datrackingui/modules/applicationmaster/default.aspx?page=found&1=thismonth&4a=9&6=F"
 
 agent = Mechanize.new
@@ -29,6 +45,17 @@ form = page.forms.first
 button = form.button_with(value: " I Agree")
 form.submit(button)
 # It doesn't even redirect to the correct place. Ugh
-
 page = agent.get(url)
-scrape_page(page)
+current_page_no = 1
+next_page_link = true
+
+while next_page_link
+  scrape_page(page)
+
+  next_page_link = page.at(".rgNumPart").search("a").find{|a| a.inner_text == (current_page_no + 1).to_s}
+  if next_page_link
+    current_page_no += 1
+    puts "Getting page #{current_page_no}..."
+    page = click(page, next_page_link)
+  end
+end
